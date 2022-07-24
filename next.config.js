0245e3602d450ad428104849850a37a8927a4e88
quibler <ashwinidroid@gmail.com>
2022-07-24 15:09:39 +0530

    section 1 mini-cv complete

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['gfcl2wgmhponogmj4us3lpnpzozvatwddwu7guc335wqpsvq.arweave.net'],
+  },
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      lineHeight: {
+        "extra-loose": "2.5",
+        12: "3rem",
+      }, 
+    },
+  },
+  plugins: [],
+};
